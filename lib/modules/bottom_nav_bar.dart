2c10c81c6097a199d1aa873b881ed8cacddbf25f
Alexandru-Nicolae Vladut <vladut.alexandru12@gmail.com>
2025-06-06 23:52:27 +0300

    update nav bar design

--- lib/modules/bottom_nav_bar.dart
+++ lib/modules/bottom_nav_bar.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app_base/core/theme/login_theme.dart';
 import 'package:flutter_app_base/modules/home/pages/home_page.dart';
 import 'package:flutter_app_base/modules/leaderboard/pages/leaderboard_page.dart';
 import 'package:flutter_app_base/modules/profile/pages/profile_page.dart';
@@ -12,13 +11,13 @@ class BottomNavBar extends StatefulWidget {
   State<BottomNavBar> createState() => _BottomNavBarState();
 }
 
-class _BottomNavBarState extends State<BottomNavBar>
-    with TickerProviderStateMixin {
+class _BottomNavBarState extends State<BottomNavBar> with TickerProviderStateMixin {
   int _selectedIndex = 0;
 
   List<Widget> widgetOptions = [];
 
   AnimationController? animationController;
+  late AnimationController _iconAnimationController;
 
   @override
   void initState() {
@@ -27,10 +26,18 @@ class _BottomNavBarState extends State<BottomNavBar>
       vsync: this,
     );
 
+    _iconAnimationController = AnimationController(
+      duration: const Duration(milliseconds: 300),
+      vsync: this,
+    );
+    
+    // Start with animation completed for initial tab
+    _iconAnimationController.value = 1.0;
+
     setState(() {
       widgetOptions = <Widget>[
         HomePage(animationController: animationController),
-        VouchersPage(),
+        const VouchersPage(),
         const LeaderboardPage(),
         const ProfilePage()
       ];
@@ -42,48 +49,106 @@ class _BottomNavBarState extends State<BottomNavBar>
   @override
   void dispose() {
     animationController?.dispose();
+    _iconAnimationController.dispose();
     super.dispose();
   }
 
   void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
+    if (_selectedIndex != index) {
+      _iconAnimationController.reset();
+      _iconAnimationController.forward();
+      setState(() {
+        _selectedIndex = index;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(child: widgetOptions.elementAt(_selectedIndex)),
-      bottomNavigationBar: BottomNavigationBar(
-        iconSize: 24,
-        backgroundColor: Colors.white,
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        type: BottomNavigationBarType.fixed,
-        selectedItemColor: kBackgroundColor, // Repository.selectedItemColor(context),
-        unselectedItemColor: Colors.grey.withOpacity(0.7),
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home_rounded),
-            label: 'Home',
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.06),
+              blurRadius: 12,
+              offset: const Offset(0, -5),
+            ),
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.redeem_rounded),
-            label: 'Marketplace',
+          child: BackdropFilter(
+            filter: ColorFilter.mode(
+              Colors.white.withOpacity(0.8),
+              BlendMode.srcOver,
+            ),
+            child: BottomNavigationBar(
+              elevation: 0,
+              backgroundColor: Colors.white.withOpacity(0.9),
+              showSelectedLabels: true,
+              showUnselectedLabels: true,
+              selectedLabelStyle: const TextStyle(
+                fontSize: 11,
+                fontWeight: FontWeight.w500,
+              ),
+              unselectedLabelStyle: const TextStyle(
+                fontSize: 10,
+                fontWeight: FontWeight.w400,
+              ),
+              type: BottomNavigationBarType.fixed,
+              selectedItemColor: Theme.of(context).primaryColor,
+              unselectedItemColor: Colors.grey.withOpacity(0.7),
+              items: [
+                _buildNavItem(Icons.home_rounded, 'Home', 0),
+                _buildNavItem(Icons.redeem_rounded, 'Vouchers', 1),
+                _buildNavItem(Icons.leaderboard_rounded, 'Leaderboard', 2),
+                _buildNavItem(Icons.person_rounded, 'Profile', 3),
+              ],
+              currentIndex: _selectedIndex,
+              onTap: _onItemTapped,
+            ),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.leaderboard_rounded),
-            label: 'Leaderboard',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person_rounded),
-            label: 'Profile',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
+        ),
       ),
+    );
+  }
+
+  BottomNavigationBarItem _buildNavItem(IconData icon, String label, int index) {
+    final isSelected = _selectedIndex == index;
+    
+    return BottomNavigationBarItem(
+      icon: Container(
+        margin: const EdgeInsets.only(bottom: 4),
+        child: AnimatedBuilder(
+          animation: _iconAnimationController,
+          builder: (context, child) {
+            return Transform.scale(
+              scale: isSelected 
+                  ? 1.0 + (_iconAnimationController.value * 0.15) 
+                  : 1.0,
+              child: Container(
+                padding: const EdgeInsets.all(6),
+                decoration: BoxDecoration(
+                  color: isSelected 
+                      ? Theme.of(context).primaryColor.withOpacity(0.1) 
+                      : Colors.transparent,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Icon(
+                  icon,
+                  size: isSelected ? 24 : 22,
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+      label: label,
     );
   }
 }
